a7f86589d4b7bd696bacc460f05127145bd4c6b6
Nicholas Galasinao <nicholas.galasinao@gmail.com>
2014-06-03 12:30:07 -0400

    stuff

--- server/register.php
+++ server/register.php
@@ -10,15 +10,17 @@ mysql_select_db($database, $con);
 $sql = "CREATE TABLE IF NOT EXISTS 'users' (
        'username' text,
        'password' text,
+       'name' text,
        PRIMARY KEY ('username)
        )"
 mysql_query($sql);
 	   
 $name = $_POST["username"];
 $pw = $_POST["password"];
+$realname = $_POST["name"];
 
-$sql = "INSERT INTO users (username, password)
-       VALUES ($name, $pw)";
+$sql = "INSERT INTO users (username, password, name)
+       VALUES ($name, $pw, $realname)";
 mysql_query($sql);
 
 mysql_close($con);
