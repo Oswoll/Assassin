2472191070095454eb019c69217a8ac435f0a75d
Nicholas Galasinao <nicholas.galasinao@gmail.com>
2014-06-18 03:18:10 -0400

    I hate php

--- server/register.php
+++ server/register.php
@@ -1,27 +1,28 @@
 <?php 
-$server = "";
-$username = "";
-$password = "";
-$database = "";
+$db_server = "db4free.net";
+$db_user = "jakeoswoll";
+$db_pass = "assassin";
+$database = "assassin";
 
-$con = mysql_connect($server, $username, $password);
-mysql_select_db($database, $con);
+$con = mysqli_connect($db_server, $db_user, $db_pass);
    
-$sql = "CREATE TABLE IF NOT EXISTS 'users' (
-       'username' text,
-       'password' text,
-       'name' text,
-       PRIMARY KEY ('username)
-       )"
-mysql_query($sql);
-	   
-$name = $_POST["username"];
-$pw = $_POST["password"];
-$realname = $_POST["name"];
+//$sql = "CREATE TABLE IF NOT EXISTS 'users' (
+//       'username' text,
+//       'password' text,
+//       'name' text,
+//       PRIMARY KEY (username)
+//       )";
 
-$sql = "INSERT INTO users (username, password, name)
-       VALUES ($name, $pw, $realname)";
-mysql_query($sql);
+//mysqli_query($sql);
 
-mysql_close($con);
+   
+//$name = $_POST["username"];
+//$pw = $_POST["password"];
+//$realname = $_POST["name"];
+
+//$sql = "INSERT INTO users (username, password, name)
+//       VALUES ($name, $pw, $realname)";
+//mysql_query($sql);
+
+mysqli_close($con);
 ?>		
